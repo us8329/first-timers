c81eb9c8ec6be8668bd0d2811abf7f9d635f369b
us8329 <utkarshsinhap1@gmail.com>
2020-07-01 21:20:54 +0530

    Update Factorial.py
    improved version of factorial question

--- Factorial.py
+++ Factorial.py
@@ -6,7 +6,7 @@ def factorial(n):
 	return 1 if (n==1 or n==0) else n * factorial(n - 1); 
 
 # Driver Code 
-num = 5; 
+num = int(input("enter number"))
 print("Factorial of",num,"is", 
 factorial(num)) 
 
